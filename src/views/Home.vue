6f57d6cc3ff88f039cc2c2393bde36c3e7af9287
ymoutai <ymoutai1027@gmail.com>
2020-12-20 20:22:33 +0900

    vue-router install

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test msg="Welcome to Your Test.js Test"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Test from '@/components/Test.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Test
+  }
+}
+</script>
