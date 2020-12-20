2f354bc5831eb711f7324eee4759b611e396e99b
ymoutai <ymoutai1027@gmail.com>
2020-12-21 01:29:44 +0900

    google map pin comp next laravel api

--- src/components/MapsLoader.vue
+++ src/components/MapsLoader.vue
@@ -1,7 +1,50 @@
 <template>
-  <v-app>
-    <v-main>
-      
-    </v-main>
-  </v-app>
+  <div>
+    <div class="google-map" ref="googleMap"></div>
+  </div>
 </template>
+
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+
+export default {
+  name: 'MapsLoader',
+
+  props: {
+    mapConfig: Object,
+    apiKey: String,
+  },
+
+  data() {
+    return {
+      google: null,
+      map: null,
+    }
+  },
+
+  async mounted() {
+    const googleMapApi = await GoogleMapsApiLoader({
+      apiKey: this.apiKey
+    })
+    this.google = googleMapApi
+    this.initializeMap()
+  },
+
+  methods: {
+    initializeMap() {
+      const mapContainer = this.$refs.googleMap
+      this.map = new this.google.maps.Map(
+        mapContainer, this.mapConfig
+      )
+      this.$emit("initialized", { api: this.google, map: this.map })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.google-map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -1,7 +1,56 @@
 <template>
-    <v-app>
-      <v-main>
-        <h1>Map</h1>
-      </v-main>
-    </v-app>
+  <v-app>
+    <v-main>
+      <h1>Map</h1>
+      <MapsLoader
+        :mapConfig="mapConfig"
+        :apiKey="apiKey"
+        @initialized="mapInitialized"
+      />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import MapsLoader from "@/components/MapsLoader.vue";
+
+export default {
+  name: "Maps",
+
+  data() {
+    return {
+      mapConfig: {
+        center: {
+          lat: 35.68944,
+          lng: 139.69167,
+        },
+        zoom: 17,
+      },
+      apiKey: process.env.VUE_APP_GOOGLE_MAP_API,
+    };
+  },
+
+  components: {
+    MapsLoader,
+  },
+
+  methods: {
+    mapInitialized: ({ api, map }) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        var latLng = new api.maps.LatLng(position.coords.latitude, position.coords.longitude, false);
+        map.setCenter(latLng);
+
+        const center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        new api.maps.Marker({
+          position: center,
+          map: map
+        });
+      });
+    },
+  },
+};
+</script>
